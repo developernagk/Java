36f9ad849c7fd26e1be062bafe2a5930ffbc1913
경 <106065973+developernagk@users.noreply.github.com>
2024-02-08 17:27:12 +0900

    Update Pcce02.java

--- javaProgrammers/src/pcce/Pcce02.java
+++ javaProgrammers/src/pcce/Pcce02.java
@@ -7,17 +7,17 @@ public class Pcce02 {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int c = sc.nextInt();
+      		int a = sc.nextInt();
+        	int c = sc.nextInt();
 
-        int b_square = (int) (Math.pow(c, 2) - Math.pow(a, 2));
-        // int b_square = c**2 - a**2;
-        // 아래 코드는 수정 전 코드
-        // int b_square = c - a;
+        	int b_square = (int) (Math.pow(c, 2) - Math.pow(a, 2));
+        	// int b_square = c**2 - a**2;
+        	// 아래 코드는 수정 전 코드
+        	// int b_square = c - a;
 
-        System.out.println(b_square);
+        	System.out.println(b_square);
         
-        sc.close();
+        	sc.close();
 
 	}
 
